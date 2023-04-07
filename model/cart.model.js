1e9b3275e2ac8988f25ba1c02e03108244e75fa9
Ayush <aryansharma7863@gmail.com>
2023-04-07 13:02:36 +0530

    few

--- model/cart.model.js
+++ model/cart.model.js
@@ -1,8 +1,10 @@
 const mongoose=require("mongoose")
 
 const userSchema=mongoose.Schema({
+    title:String,
     image:String,
     price:Number,
+    size:Number,
     discounted_price:Number
 })
 
